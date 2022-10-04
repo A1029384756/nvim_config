4eb8d454c6e8d51e89088841f1f5c9aa8d999623
Hayden Gray <35206453+A1029384756@users.noreply.github.com>
2022-10-04 13:44:15 -0400

    added toggleterm

--- init.lua
+++ init.lua
@@ -8,17 +8,3 @@ require('plugin_config.lsp')
 require('plugin_config.telescope')
 require('plugin_config.toggleterm')
 
-local theme_present, theme = pcall(require, 'one_monokai')
-if theme_present then
-  theme.setup({
-    themes = function ()
-      return {
-        Normal = { bg = '#1F2532'},
-        Comment = { fg = '#95a5a6', italic = true },
-        ErrorMsg = { fg = "black", bg = "#ec6075", standout = true },
-        NormalFloat = { link = "Normal" },
-      }
-    end,
-  })
-end
-
--- lua/haydengray/vimopts.lua
+++ lua/haydengray/vimopts.lua
@@ -7,5 +7,19 @@ vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
-vim.cmd([[colorscheme one_monokai]])
+
+local theme_present, theme = pcall(require, 'one_monokai')
+if theme_present then
+  theme.setup({
+    themes = function ()
+      return {
+        Normal = { bg = '#1F2532'},
+        Comment = { fg = '#95a5a6', italic = true },
+        ErrorMsg = { fg = "black", bg = "#ec6075", standout = true },
+        NormalFloat = { link = "Normal" },
+      }
+    end,
+  })
+  vim.cmd([[colorscheme one_monokai]])
+end
 
