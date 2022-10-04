d8c69c64fbf58525bdf3afdc84e545d5dd1be45d
Hayden Gray <35206453+A1029384756@users.noreply.github.com>
2022-10-04 13:54:02 -0400

    added harpoon check

--- lua/haydengray/mappings.lua
+++ lua/haydengray/mappings.lua
@@ -1,14 +1,20 @@
 vim.g.mapleader = ' '
 
+local telescope_present, builtin = pcall(require, 'telescope.builtin')
+if not telescope_present then
+  return
+end
+local harpoon_present = pcall(require, 'harpoon')
+if not harpoon_present then
+  return
+end
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 vim.keymap.set('n', '<leader>mf', require('harpoon.mark').add_file, {})
 vim.keymap.set('n', '<leader>rm', require('harpoon.mark').rm_file, {})
 vim.keymap.set('n', '<leader>hf', require('harpoon.ui').toggle_quick_menu, {})
 vim.keymap.set('n', '<leader>fh', ':Telescope harpoon marks<CR>', {})
-
-local telescope_present, builtin = pcall(require, 'telescope.builtin')
-if telescope_present then
-  vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-end
