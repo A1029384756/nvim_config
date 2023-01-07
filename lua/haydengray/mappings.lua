68c60718de8dee0d2fc1f9b454cf8c53ad4cc5b3
Hayden Gray <35206453+A1029384756@users.noreply.github.com>
2023-01-06 21:39:38 -0500

    Removed folding

--- lua/haydengray/mappings.lua
+++ lua/haydengray/mappings.lua
@@ -2,9 +2,6 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>fe', ':E<CR><CR>', {})
 vim.keymap.set('n', '<esc>', ':noh<CR>', {})
 
---folding
-vim.keymap.set('n', '<leader>sf', 'za', {})
-
 local telescope_present, builtin = pcall(require, 'telescope.builtin')
 if not telescope_present then
   return
